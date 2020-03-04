5aa1840cd3b8aefd7b8bb8e3157fddc52c34f693
Brandon Sanchez <bssanchez93@gmail.com>
2020-03-03 20:40:50 -0500

    [*] Created ContactType and use symfony/forms for create contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -12,8 +15,38 @@ class ContactController extends AbstractController
      */
     public function index()
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
         return $this->render('contact/index.html.twig', [
             'title' => 'CONTÁCTANOS',
+            'form' => $form->createView()
         ]);
+    }
+
+    public function save(Request $request)
+    {
+        $contact = new Contact();
+        $contact->setSendDate(new \DateTime('now'));
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            
+            // $contact = $form->getData();
+            // echo "<pre>";
+            // print_r($contact);
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+            // die('seeeee');
+            return $this->redirectToRoute('contact');
+        }
     }
 }
