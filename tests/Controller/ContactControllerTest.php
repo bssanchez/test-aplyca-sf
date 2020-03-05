9f51235136880ee0b6d26e7ee84bed86ba76cbbc
Brandon Sanchez <bssanchez93@gmail.com>
2020-03-04 19:05:44 -0500

    [*] Finished contact functionals

--- tests/Controller/ContactControllerTest.php
+++ tests/Controller/ContactControllerTest.php
@@ -6,12 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ContactControllerTest extends WebTestCase
 {
-    public function testSomething()
+    private $url = '/contact-us';
+
+    /**
+     * @description Check route is OK 
+     */
+    public function testIndexResponseOK()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', $this->url);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+    /**
+     * @description Check if title is 'BLOG'
+     */
+    public function testIndexTitle()
+    {
+        $client = static::createClient();
+        $client->request('GET', $this->url);
+
+        $this->assertSelectorTextSame('html h1', 'CONTÁCTANOS');
     }
 }
